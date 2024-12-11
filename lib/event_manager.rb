bd5ed205f21c5392fa53ad576d3f48f97036a846
Beno <chocomilo451@gmail.com>
2024-12-11 16:11:54 +0000

    Form Letter Added

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,33 +1,41 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-puts "Event Manager Initialized"
-
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = File.read("../civic_info.key").strip
-
-contents = CSV.open(
-  '../event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
+puts 'Event Manager Initialized'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0...5]
 end
 
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = File.read('../civic_info.key').strip
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislator_names.join(', ')
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+template_letter = File.read('../form_letter.html')
+
+contents = CSV.open('../event_attendees.csv',
+                    headers: true,
+                    header_converters: :symbol)
+
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-begin
-  legislators = civic_info.representative_info_by_address(
-    address: zipcode,
-    levels: 'country',
-    roles: ['legislatorUpperBody', 'legislatorLowerBody']
-  )
-  legislators = legislators.officials
-rescue
-      'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-end
-  puts "#{name}: #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+  personal_letter = template_letter.gsub('FIRST_NAME', name)
+  personal_letter.gsub!('LEGISLATORS', legislators)
+  puts personal_letter
 end
